dcd3ecc74fd305c46f63f56bd98036b653ba69f8
Siolamo <oibaf250407@gmail.com>
2026-09-21 15:00:35 +0200

    first commit

---
+++ basics/class 101/Main.java
@@ -0,0 +1,23 @@
+/*
+    Java: OOP: Object Oriented Programming
+    1st Regola d'oro: IL NOME DI OGNI CLASSE DEV'ESSERE UGUALE AL NOME DEL FILE
+    Main.java = public class Main
+    Java è Case Sensitive
+
+*/
+
+/*
+ Saltare questa parte se avete già installato java
+
+ come installare java
+ Windows: cercare su google Java download e scaricare un javaJDK di versione sopra alla 11
+ to be continued
+ */
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello world!");
+    }
+}
