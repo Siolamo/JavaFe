3c3a4d5a4a92bd135365c6150b0f994c42931a4b
Siolamo <oibaf250407@gmail.com>
2026-09-22 10:48:17 +0200

    Poesia

--- basics/class 101/Main.java
+++ basics/class 101/Main.java
@@ -1,4 +1,16 @@
 /*
+ Saltare questa parte se avete già installato java
+
+ come installare java
+ Windows: cercare su google Java download e scaricare un javaJDK di versione sopra alla 11
+ Mac: leggere quello scritto per Windows
+ Linux: Debian based: sudo apt install openjdk-21-jdk
+ arch based: sudo pacman -S jdk21-openjdk
+ 
+ */
+
+
+ /*
     Java: OOP: Object Oriented Programming
     1st Regola d'oro: IL NOME DI OGNI CLASSE DEV'ESSERE UGUALE AL NOME DEL FILE
     Main.java = public class Main
@@ -7,12 +19,23 @@
 */
 
 /*
- Saltare questa parte se avete già installato java
+OOP, perchè la si usa la OOP
+La programmazione ad oggetti viene usata 
+Perché l'ingegno umano, in sé disperso,
+Nel gran mar de la selva di funzioni,
+Ritrovi l’ordine in quel caos avverso.
 
- come installare java
- Windows: cercare su google Java download e scaricare un javaJDK di versione sopra alla 11
- to be continued
- */
+Raggruppa i dati e le operazioni
+In vive forme, sì che l'intelletto
+Non tremi dinanzi alle complicazioni.
+
+L'arte che move il codice perfetto
+Sì come l'universo il Primo Amore,
+Dando a ciascuna classe il suo effetto.
+*/
+
+
+
 
 public class Main
 {
